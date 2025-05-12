78a7212f56177100be19f601eb6a8662e57432e4
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-05-12 17:58:02 +0200

    Add: milestone 2 and milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [addedProducts, setAddedProducts] = useState([])
 
   const products = [
     { name: 'Mela', price: 0.5 },
@@ -10,17 +10,62 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
+  const addToCart = (product) => {
+    const addedProduct = addedProducts.find(e => e.name === product.name)
+    if(addedProduct){
+      updateProductQuantity(addedProduct.name, addedProduct.quantity + 1)
+      return
+    }
+      setAddedProducts(curr => [...curr, {
+        ...product,
+        quantity: 1
+      }])
+  }
+
+  const updateProductQuantity = (name, quantity) => {
+    setAddedProducts(curr => curr.map(p => p.name === name ? {...p, quantity} : p))
+  }
+
+  const removeFromCart = product => {
+    setAddedProducts(curr => curr.filter(p => p.name !== product.name))
+  }
+
+  const totale = addedProducts.reduce((acc, element) => {
+    return acc + (element.price * element.quantity)
+  }, 0)
+
   return (
+    <>
     <div>
       <h1>Lista prodotti</h1>
       <ul>
-        {products.map((e, i) => {
-          return <li key={i}>{e.name}: {e.price.toFixed(2)}€</li>
+        {products.map((product, index) => {
+          return (
+            <li key={index}>{product.name}: {product.price.toFixed(2)}€
+              <button
+              onClick={() => addToCart(product)}>Aggiungi al carrello</button>
+            </li>
+          )
         }
         )}
       </ul>
-
     </div>
+    
+    <div>
+      <h1>Carrello</h1>
+      <ul>
+      {addedProducts.map((element, index) => {
+        return (
+          <li key={index}>{element.quantity} x {element.name} - {element.price.toFixed(2)}€
+          <button onClick={() => removeFromCart(element)}>Rimuovi dal carrello</button>
+          </li>
+        )
+      })}
+      <h2>Totale: {totale.toFixed(2)}€</h2>
+      </ul>
+    </div>
+    </>
+    
   )
 }
 
